e77155311ac733c2d7d1788f0a0751d2b245ae37
mahdellorti <00001069563717sp@al.educacao.sp.gov.br>
2024-11-07 11:08:28 -0300

    projeto 3 cartas

---
+++ Projeto 3-20241107T140631Z-001/Projeto 3/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'The office',
+    'Em que temporada o Jim e a Pam ficam juntos?',
+    '3 Temporada'
+)
+
+criaCartao(
+    'B99',
+    'Qual a sexualidade da Rosa Dias',
+    'Bissexual'
+)
+
+criaCartao(
+    'Everthing Sucks',
+    'Qual a descoberta que o Luke faz na viagem para o filme?',
+    'Encontra seu pai'
+)
+
+criaCartao(
+    'Atypical',
+    'Qual esporte a cassie pratica?',
+    'Corrida'
+)
